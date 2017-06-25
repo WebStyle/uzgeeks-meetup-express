56852de286b6ee1ec9719ec186f600d8b01e4086
Farrukh <webstyle@protonmail.com>
2017-06-25 21:26:45 +0500

    refactor structure

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Events = require('./Events');
+const events = require('events');
 
 const app = express();
+const Events = new events();
 
-app.use(Events.accept);
-
-app.get('/', (req, res) => {
+app.get('/', guard, (req, res) => {
   console.log('request');
   res.json({ message: 'Hello you are accepted' })
 });
+
+
+app.get('/accept', (req, res) => {
+  Events.emit('accept');
+  res.json({ message: 'accept sent' })
+});
+
+function guard(req, res, next) {
+  Events.on('accept', () => next());
+}
 
 app.listen(3000, () => console.log('Server is run on port 3000'));
